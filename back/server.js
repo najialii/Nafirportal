e098154f3a0b3f258690aec68d9faa08bcaf2bc6
Mohamed abdallah <57531826+mohamedabdallah09209@users.noreply.github.com>
2025-03-14 03:54:52 -0700

    solve server file conflict

--- back/server.js
+++ back/server.js
@@ -7,10 +7,19 @@ const { Server } = require('socket.io');
 const path = require('path');
 const cvRoutes = require('./routes/cvrouts');
 const activityRoutes = require('./routes/activityRoutes');
-
 const departmentRoutes = require('./routes/departmentRoutes');
+const mentorsessions = require('./routes/mentorSessions');
+const convroutes = require('./routes/convroutes');
+const Message = require('./routes/msgroutes');
+const menreq = require('./routes/menSesReq');
+const userRoutes = require('./routes/userRoutes');
+const chatRoutes = require('./routes/chatRoute');
+const connectionRoutes = require('./routes/connectRouter');
+const blogs = require('./routes/blogsRoutes');
+const act = require('./routes/activityRoutes');
+
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '1000mb' }));
 app.use(cors({
     origin: "http://localhost:5173",
     methods: ["GET", "POST", "PATCH"],
@@ -34,14 +43,6 @@ app.use('/api/cvs', cvRoutes);
 app.use('/api/activities', activityRoutes);
 
 // Other routes...
-const mentorsessions = require('./routes/mentorSessions');
-const convroutes = require('./routes/convroutes');
-const Message = require('./routes/msgroutes');
-const menreq = require('./routes/menSesReq');
-const userRoutes = require('./routes/userRoutes');
-const chatRoutes = require('./routes/chatRoute');
-const connectionRoutes = require('./routes/connectRouter');
-
 app.use('/api/mentorsessions', mentorsessions);
 app.use("/api/requestsession", menreq);
 app.use("/api/user", userRoutes);
@@ -50,162 +51,78 @@ app.use("/api/messages", Message);
 app.use("/api/connect", connectionRoutes);
 app.use('/api/department', departmentRoutes);
 app.use('/api/activity', activityRoutes);
+app.use("/api/blogs", blogs);
+app.use("/api/act", act);
 
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-    const app = express();
-
-
-
-    app.use(express.json({ limit: '1000mb' }));
-
-    app.use(cors({
         origin: "http://localhost:5173",
         methods: ["GET", "POST"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true
-    }));
-
-    const mentorsessions = require('./routes/mentorSessions');
-    const convroutes = require('./routes/convroutes')
-    const Message = require('./routes/msgroutes')
-
-
-    const menreq = require('./routes/menSesReq');
-    const userRoutes = require('./routes/userRoutes');
-    const cvRoutes = require("./routes/cvrouts");
-    const chatRoutes = require('./routes/chatRoute');
-    const blogs = require('./routes/blogsRoutes');
-    const act = require('./routes/activityRoutes') 
-    // const Chat = require('./models/chat'); // Import the Chat model
-const connectionRoutes = require('./routes/connectRouter')
-
-
-
-    const server = http.createServer(app);
-    const io = new Server(server, {
-        cors: {
-            origin: "http://localhost:5173",
-            methods: ["GET", "POST"],
-            allowedHeaders: ["Content-Type", "Authorization"],
-            credentials: true
-        }
-    });
-
-
-    app.use((req, res, next) => {
-        console.log('Middleware executed');
-        console.log(req.path, req.method);
-        next();
-    });
-
-    // Routes
-    app.use('/api/mentorsessions', mentorsessions);
-    app.use("/api/requestsession", menreq);
-    app.use("/api/user", userRoutes);
-    app.use("/api/cvs", cvRoutes);
-  //  app.use("/api/chat", chatRoutes);
-    app.use("/api/conversation", convroutes);
-    app.use("/api/messages", Message);
-    app.use("/api/connect", connectionRoutes);
-    app.use('/api/department', departmentRoutes);
-    app.use('/api/activity', activityRoutes);
-    
-    
-    app.use("/api/blogs", blogs);
-    app.use("/api/act", act);
-
-
-
-    // chat functionality  
-    let users = [] 
-    const addUser = (userId, socketId) => {
-        
-        if (!users.some(user => user.userId === userId)) {
-            users.push({ userId, socketId });
-        } else {
-            // If the user  existsupdate their socket ID
-            users = users.map(user =>
-                user.userId === userId ? { userId, socketId } : user
-            );
-        }
-        console.log('here are the users array',users)
-    };
-    
-    
-   
-    const RemoveUser = (socketId)=>{
-        users = users.filter(user => user.socketId !== socketId)
     }
 });
 
-// chat functionality  
-let users = [] 
+// Chat functionality
+let users = [];
 const addUser = (userId, socketId) => {
-    
     if (!users.some(user => user.userId === userId)) {
         users.push({ userId, socketId });
     } else {
-        // If the user  existsupdate their socket ID
+        // If the user exists, update their socket ID
         users = users.map(user =>
             user.userId === userId ? { userId, socketId } : user
         );
     }
-    console.log('here are the users array',users)
+    console.log('here are the users array', users);
 };
 
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+};
 
-const RemoveUser = (socketId)=>{
-    users = users.filter(user => user.socketId !== socketId)
-}
+const getUser = (userId) => {
+    return users.find(user => user.userId === userId);
+};
 
-const getUser = (userId)=>{
-    return users.find(user => user.userId === userId)
-}
-io.on('connection', (socket)=>{
-    console.log(`user conected with soc id : ${socket.id}`);
+io.on('connection', (socket) => {
+    console.log(`user connected with socket id: ${socket.id}`);
 
-    //take  user id and socket id from user
-socket.on('add-user', (userId)=>{    
-console.log(`User added: ${userId}, Socket ID: ${socket.id}`); 
-addUser(userId,socket.id)
-io.emit("get-users", users)
-})
+    // Take user id and socket id from user
+    socket.on('add-user', (userId) => {
+        console.log(`User added: ${userId}, Socket ID: ${socket.id}`);
+        addUser(userId, socket.id);
+        io.emit("get-users", users);
+    });
 
-    // join
-socket.on("join-room", (data) => {
-socket.join(data)
-console.log(`user ${socket.id} joined room ${data}`)
-})
+    // Join room
+    socket.on("join-room", (data) => {
+        socket.join(data);
+        console.log(`user ${socket.id} joined room ${data}`);
+    });
 
-    
-    // send and get  msg
-socket.on('send-message' , ({senderId, receiverId, text})=>{
-const user = getUser(receiverId)
-console.log('sent message data', senderId, receiverId, text)
+    // Send and get message
+    socket.on('send-message', ({ senderId, receiverId, text }) => {
+        const user = getUser(receiverId);
+        console.log('sent message data', senderId, receiverId, text);
 
-if(!user){
-console.log('user not found in the listtttttttttttt' )
-return
-}
-io.to(user.socketId).emit('get-messages',{
-senderId, 
-text
-})
+        if (!user) {
+            console.log('user not found in the list');
+            return;
+        }
+        io.to(user.socketId).emit('get-messages', {
+            senderId,
+            text
+        });
+    });
 
-// console.log(text)
-    // console.log(data)
-//   socket.to(data.room).emit('receive-message', data)
-} )
-    socket.on('disconnect', () =>{
-        RemoveUser(socket.id)
-        console.log(`user disconnected with the id : ${socket.id}`)
-        //i have no clue tbh 
-        io.emit("get-users", users) 
-    })
-    
-})
+    socket.on('disconnect', () => {
+        removeUser(socket.id);
+        console.log(`user disconnected with the id: ${socket.id}`);
+        io.emit("get-users", users);
+    });
+});
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI)
